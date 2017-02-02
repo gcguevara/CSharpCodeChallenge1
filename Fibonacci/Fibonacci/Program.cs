7139b8f343414f098988282658d01864f8bd8dc5
gcguevara <garrettcguevara@gmail.com>
2017-02-01 19:31:56 -0800

    Initial commit.

---
+++ Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        /* *****CODE CHALLENGE #20*****
+         * 
+         *  20. Write a function that computes the list of the first 100 Fibonacci numbers.
+         * 
+         */
+
+        /// <summary>
+        /// Iterative approach
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public static List<int> fibonacci(int num)
+        {
+            int first = 0;
+            int second = 1;
+            int result = 0;
+
+            List<int> fibList = new List<int>(100) { 0, 1 };
+
+            // run checks for if num is equal to 0 or 1
+            if (num == 0)
+            {
+                Console.WriteLine("You must pass in a value two or greater.");
+                return fibList;
+            }
+
+            if (num == 1)
+            {
+                Console.WriteLine("You must pass in a value two or greater.");
+                return fibList;
+            }
+
+            // for i is at least two and less than num
+            for (int i = 2; i < num; i++)
+            {
+                // add first and second
+                result = first + second;
+                fibList.Add(result);
+
+                // assign the value of second to first
+                first = second;
+
+                // assign the sum to second
+                second = result;
+            }
+
+            return fibList;
+        }
+
+        static void Main(string[] args)
+        {
+            foreach (var num in fibonacci(100))
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
---
+++ TwoIndexRotator/TwoIndexRotator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoIndexRotator
+{
+    class Program
+    {
+        public static List<int> twoIndexRotator(List<int> nums)
+        {
+            List<int> firstTwo = nums.GetRange(0, 2);
+            List<int> theRest = nums.GetRange(2, nums.Count() - 2);
+
+            // because there isn't a built in method to swap in C#, we'll have to manually do it
+            nums.RemoveRange(0, nums.Count());
+            nums.AddRange(theRest);
+            nums.AddRange(firstTwo);
+
+            return nums;
+        }
+
+        static void Main(string[] args)
+        {
+            List<int> example = new List<int>() { 1, 2, 3, 4, 5, 6 };
+
+            foreach (var num in twoIndexRotator(example))
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
