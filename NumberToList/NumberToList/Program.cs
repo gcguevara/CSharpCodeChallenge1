7cf73e5068713e3870c700590ea1925cb8f08778
gcguevara <garrettcguevara@gmail.com>
2017-02-01 20:29:43 -0800

    Initial commit.

---
+++ NumberToList/NumberToList/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberToList
+{
+    class Program
+    {
+        /* *****CODING CHALLENGE #21*****
+         * 
+         * 21. Write a function that takes a number and returns a list of its digits.
+         * 
+         */
+
+        public static List<double> numberToList(string num)
+        {
+            List<double> newList = new List<double>();
+            char[] tempArr;
+            
+            tempArr = num.ToCharArray();
+            foreach (var value in tempArr)
+            {
+                double digit = char.GetNumericValue(value);
+                newList.Add(digit);
+            }
+
+            return newList;
+        }
+
+        static void Main(string[] args)
+        {
+            foreach (var value in numberToList("12345"))
+            {
+                Console.WriteLine(value); // { 1, 2, 3, 4, 5 }
+            }
+        }
+    }
+}
