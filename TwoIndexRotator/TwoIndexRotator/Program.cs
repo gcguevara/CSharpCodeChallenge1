c46811b64e84f267be0a41f3ddcbc79edd37899d
gcguevara <garrettcguevara@gmail.com>
2017-02-01 21:12:07 -0800

    Corrected listRotator and added input checks.

--- TwoIndexRotator/TwoIndexRotator/Program.cs
+++ TwoIndexRotator/TwoIndexRotator/Program.cs
@@ -4,28 +4,60 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TwoIndexRotator
+namespace ListRotator
 {
     class Program
     {
-        public static List<int> twoIndexRotator(List<int> nums)
+        /* *****CODING CHALLENGE #21*****
+         * 
+         * Write a function that rotates a list by k elements. For example [1,2,3,4,5,6] rotated by two becomes [3,4,5,6,1,2].
+         * Try solving this without creating a copy of the list. How many swap or move operations do you need?
+         * 
+         */
+
+        public static void listRotator(List<int> nums, int k)
         {
-            List<int> firstTwo = nums.GetRange(0, 2);
-            List<int> theRest = nums.GetRange(2, nums.Count() - 2);
+            if(k < 0)
+            {
+                k %= nums.Count();
+            }
 
-            // because there isn't a built in method to swap in C#, we'll have to manually do it
-            nums.RemoveRange(0, nums.Count());
-            nums.AddRange(theRest);
-            nums.AddRange(firstTwo);
+            if (k == 0)
+            {
+                Console.WriteLine("Why would you rotate a list by zero?");
+            }
 
-            return nums;
+            if (k > nums.Count())
+            {
+                k %= nums.Count();
+            }
+
+            int index = k;
+            int temp;
+
+            while (index < nums.Count())
+            {
+                int swapIndex = index - k;
+                temp = nums[index];
+                nums[index] = nums[swapIndex];
+                nums[swapIndex] = temp;
+                index++;
+            }
         }
 
         static void Main(string[] args)
         {
             List<int> example = new List<int>() { 1, 2, 3, 4, 5, 6 };
 
-            foreach (var num in twoIndexRotator(example))
+            Console.WriteLine("Before rotation:\n");
+            foreach (var num in example)
+            {
+                Console.WriteLine(num);
+            }
+
+            listRotator(example, 2);
+            Console.WriteLine("\nAfter rotation:\n");
+            foreach (var num in example)
             {
                 Console.WriteLine(num);
             }
